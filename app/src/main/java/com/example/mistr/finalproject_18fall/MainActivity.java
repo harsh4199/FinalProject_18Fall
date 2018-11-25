5510d5ae03875f99264840b1cedcc4e47c4e5530
mistr <mistry4199@gmail.com>
2018-11-25 12:52:45 -0500

    step1

--- app/src/main/java/com/example/mistr/finalproject_18fall/MainActivity.java
+++ app/src/main/java/com/example/mistr/finalproject_18fall/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.mistr.finalproject_18fall;
 
 import android.app.Activity;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ImageButton cbc = (ImageButton) findViewById(R.id.img_cbc);
+        Drawable d1 = Drawable.createFromPath("@drawable/cbc");
+        cbc.setImageDrawable(d1);
+
+
+        final ImageButton food = (ImageButton) findViewById(R.id.img_food);
+        Drawable d2 = Drawable.createFromPath("@drawable/cbc");
+        food.setImageDrawable(d2);
+
+        final ImageButton movie = (ImageButton) findViewById(R.id.img_movie);
+        Drawable d3 = Drawable.createFromPath("@drawable/cbc");
+        movie.setImageDrawable(d3);
+
     }
+
 }
